622b32ec9a7f0d259c6416caa218443834093de4
ksm524923 <s2619@e-mirim.hs.kr>
2026-05-28 12:56:17 +0900

    init

---
+++ javascript/change-color.js
@@ -0,0 +1,5 @@
+var heading = document.querySelector('#heading');
+heading.onlick=function() { heading.style.color="red";}
+alert("안녕하세요")
+var reply=confirm("정말 배경 이미지를 바꾸겠습니다?");
+var name=prompt("이름을 입력하세요.", "아무개");
